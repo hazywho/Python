7383673e417f0fb406f6022f3329b9586b09280e
hazywho <116442947+hazywho@users.noreply.github.com>
2023-04-22 13:22:05 +0800

    done

--- Programming tests.py
+++ Programming tests.py
@@ -1,14 +1,20 @@
-string = "^_"
-loc = 0
-times = 0
-while loc < len(string):
-    if string[loc-1] == "_" and string[loc] != "^":
-        string = string[:loc] + "^" + string[loc:]
-        print(string)
-        times +=1
+string =["^______^","___^_^^^_^___^","^_","^","^_^^^^^_^_^^","___^^","_"]
 
-    loc +=1
-    
-print(times)
 
-#string = string[:2] + "^" + string[1:]
+for cases in string:
+    count = 0
+    if cases[0] != "^":
+        count +=1
+    if cases [-1] != "^":
+        count +=1
+    if cases.count("_") + cases.count("^") == 1 and cases[0] == "^":
+        count +=1
+
+    for i in range(1, len(cases)-1):
+        if cases[i] == "_"  and cases[i-1] != "^":
+            count += 1
+    print(count)
+
+
+
+
