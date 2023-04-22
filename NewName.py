d51d7c8237ed2f4068dbe41eff79f657df53a384
hazywho <116442947+hazywho@users.noreply.github.com>
2023-04-22 13:28:23 +0800

    nothing

--- NewName.py
+++ NewName.py
@@ -1,9 +1,20 @@
-string = "^______^"
-while True:
-    for loc,x in enumerate(string):
-        if x == "_":
-            string = string[:2] + "^" + string[1:]
-            print(string)
+string =["^______^","___^_^^^_^___^","^_","^","^_^^^^^_^_^^","___^^","_"]
 
 
-        
+for cases in string:
+    count = 0
+    if cases[0] != "^":
+        count +=1
+    if cases [-1] != "^":
+        count +=1
+    if cases.count("_") + cases.count("^") == 1 and cases[0] == "^":
+        count +=1
+
+    for i in range(1, len(cases)-1):
+        if cases[i] == "_"  and cases[i-1] != "^":
+            count += 1
+    print(count)
+
+
+
+
