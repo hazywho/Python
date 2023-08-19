9f280fcb6b620a82eda32395c6be91ddd7bb83d1
hazywho <116442947+hazywho@users.noreply.github.com>
2023-08-19 11:20:44 +0800

    update

--- Volumetok.py
+++ Volumetok.py
@@ -8,8 +8,8 @@ tem = 2800/(99+273.15)
 ranges = int(input())
 
 for x in range(0,ranges):
-    celcius = int(input())
+    celcius = float(input())
     kelvin = celcius+273.15
     vballoon = kelvin*tem
     log.append(round(vballoon,7))
-print(log)
+for n in log: print(n)
