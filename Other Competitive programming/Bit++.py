4fc9811bd2cf85710017bae3233d392b21a76bb3
hazywho <116442947+hazywho@users.noreply.github.com>
2023-09-21 10:43:36 +0800

    new update

---
+++ Other Competitive programming/Bit++.py
@@ -0,0 +1,11 @@
+times = int(input())
+count = 0
+for r in range(times):
+    key = input()
+    key = key.replace("X", '')
+    if key == "--":
+        count -= 1
+    if key == "++":
+        count += 1
+
+print(count)
---
+++ Other Competitive programming/Mathematicians.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int i1,i2;
+int main(){
+    cin >> i1 >> i2;
+    cout << i1%i2 << endl;
+}
+
+// SORT FROM MIN TO MAX,
+// FIND VALID VALUES FROM THE RANGE,
+// COUNT PLUS PLUS,
