c1f2c6705081cf2a8d0f8c0176d06a399fed6f53
hazywho <116442947+hazywho@users.noreply.github.com>
2023-10-12 16:15:21 +0800

    FIXED

--- copy of he.py
+++ copy of he.py
@@ -21,7 +21,7 @@
 
 # In[3]:
 import os
-path ="/home/hihi/Downloads/ht/images" # shows dialog box and return the path
+path ="/home/hizy/Downloads/ht/images" # shows dialog box and return the path
 print(path)
 os.chdir(path)
 from unittest.util import sorted_list_difference 
@@ -39,7 +39,6 @@ import time
 import math
 import tkinter
 from tkinter import messagebox
-ifture = 0
 
 
 
@@ -54,32 +53,39 @@ def variance_of_laplacian(image):
     
 
 def run():
-    path ="/home/hihi/Downloads/ht/images"
+    path ="/home/hizy/Downloads/ht/images"
+    
     GPIO_pins = (14,15,18)
     direction = 20
     step = 21
     distance = 80 
     mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
     rotation = 0
-    while rotation < 15:
+    rs = 20
+    while rotation < rs:
         camera = cv2.VideoCapture(0)
         return_value, image = camera.read()
         cv2.imwrite(os.path.join(path,(str(rotation)+".jpg")), image)
         del(camera)
-        mymotortest.motor_go(False, "Full", 5, 0.005, False, 0.01)
-        rotation = rotation + 1 
-    mymotortest.motor_go(True, "Full", 11, 0.005, False, 0.05)
-
-    # In[4]:
-
-     
-
-    # loop over the input images
-    for imagePath in paths.list_images(path):
-        image = cv2.imread(imagePath)                      
+        mymotortest.motor_go(1, "Full", 1, 0.005, False, 0.01)
+        image = cv2.imread(os.path.join(path,(str(rotation)+".jpg")))                      
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         fm = variance_of_laplacian(gray)
+        folder = "/home/hizy/Downloads/ht/placeholder"
+        num=str(round(fm*100)/100)+".jpg"
+        cv2.imwrite(os.path.join(folder , num), image)
+        rotation += 1
+    mymotortest.motor_go(0, "Full", rs, 0.005, False, 0.01)
+        
 
+    # In[4]:
+def show():
+    path ="/home/hizy/Downloads/ht/placeholder"
+    lst = []
+        # loop over the input images
+    for imagePath in paths.list_images(path):
+        fm = round(float(Path(imagePath).stem)*100)/100
+        image = cv2.imread(imagePath)
         #blur level
         if fm < 50:
             text = "Blurry"
@@ -88,90 +94,46 @@ def run():
         # show the image
         cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
             cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 3)
-        #cv2.imshow("Image", image)
-        #key = cv2.waitKey(0)
-        #cv2.destroyWindow 
-        #typein placeholder folder directory
-        defal = "/home/hihi/Downloads/ht/Placeholder"
-        num=str(round(fm))+".jpg"
-        cv2.imwrite(os.path.join(defal , num), image)
-
-
-    list = os.listdir(defal)
-    def path (x):
-        fn = Path(x).stem
-        return fn
-    lst=[]
-    for filename in list:
-        lst.append(int(path(filename)))
-        
-
-
-
-    #type in output folder directory
-    dst_folder = "/home/hihi/Downloads/ht/Output"
-
-
-
-    file_name = str(max(lst))+".jpg"
-
-    new_name = os.path.join(dst_folder, "new_base.jpg")
-
-    shutil.move(defal + "/" + file_name, new_name )
-
-    global ifture
-    ifture = 1
-    messagebox.showinfo("Add button", "Image is done capturing")
-    
-
-    
-
-def img():
-    if ifture == 1 :
-    
-        path ="/home/hihi/Downloads/ht/images"
-        for x in paths.list_images(path):
-            
-            image = cv2.imread(x)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            fm = variance_of_laplacian(gray)
-                #blur level
-            if fm < 50:
-                text = "Blurry"
-            else:
-                text = "Not Blurry"
-                # show the image
-            cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-                cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 3)
-        
-            cv2.imshow("picture",image)
-            cv2.waitKey()
-        dir = "/home/hihi/Downloads/ht/Placeholder"
-        
-        for files in os.listdir(dir):
-            path = os.path.join(dir, files)
-            try:
-                shutil.rmtree(path)
-            except OSError:
-               os.remove(path)
- 
-        dirtwo = "/home/hihi/Downloads/ht/images"
-        for filestwo in os.listdir(dirtwo):
-            pathtwo = os.path.join(dirtwo, filestwo)
-            try:
-                shutil.rmtree(pathtwo)
-            except OSError:
-               os.remove(pathtwo)
+        cv2.imshow("Image", image)
+        key = cv2.waitKey(0)
         cv2.destroyAllWindows()
-    else:
-        return
+        lst.append(round(fm*100)/100)
+        
+    #type in placeholder folder directory
+    print(lst)
+    folder = "/home/hizy/Downloads/ht/output"
+    file_name = str(max(lst))+".jpg"
+    print(file_name)
+
+    new_name = os.path.join(folder, "new_base.jpg")
+
+    shutil.move("/home/hizy/Downloads/ht/placeholder/" + file_name, new_name )
+
+    
+def delete():
+    dir = "/home/hizy/Downloads/ht/placeholder"
+    for files in os.listdir(dir):
+        path = os.path.join(dir, files)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+           os.remove(path)
+
+    dirtwo = "/home/hizy/Downloads/ht/images"
+    for filestwo in os.listdir(dirtwo):
+        pathtwo = os.path.join(dirtwo, filestwo)
+        try:
+            shutil.rmtree(pathtwo)
+        except OSError:
+           os.remove(pathtwo)
+    cv2.destroyAllWindows()
+
 
 root = tkinter.Tk()
 b1 = tkinter.Button(root,text="add",command= run)
-b2 = tkinter.Button(root,text="show image",command= img)
+b2 = tkinter.Button(root,text="show image",command= show)
+b3 = tkinter.Button(root,text="delete images",command= delete)
+b3.place(x=100,y=100)
 b1.place(x=20,y=20)
 b2.place(x=100,y=20)
 root.mainloop()
-
-
- 
