827c1647f252d9a4aec9e8f985d80faf94bc5fde
hazywho <116442947+hazywho@users.noreply.github.com>
2023-10-19 22:41:28 +0800

    i need mentalhelp

--- Other Competitive programming/python/RestoreTheWeather.py
+++ Other Competitive programming/python/RestoreTheWeather.py
@@ -1,1 +1,20 @@
-x = int(i)
+r = int(input())
+for _ in range(r):
+    l, restrictions = list(map(int,input().split()))
+    firstl = list(map(int,input().split()))
+    secondl = list(map(int,input().split()))
+    newl = []
+    for i in firstl:
+        if i > 0:
+            secondl.sort(reverse=True)
+        else:
+            secondl.sort()
+        for s in secondl:
+            if abs(i-s) <= restrictions:
+                newl.append(s)
+                secondl.pop(secondl.index(s))
+                
+
+    for items in range(len(newl)-1): print(newl[items], end = " ")
+    print(newl[len(newl)-1])
+#need to search for all first
--- Other Competitive programming/python/tempCodeRunnerFile.py
+++ Other Competitive programming/python/tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-for index,x in enumerate(decoded):
-#     if x == " ":
-#         if x == x[index+1]:
-#             print("YES")
-#             decoded = remove_letter(decoded,x)
-# print(decoded)
+
+                break
