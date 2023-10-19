3b451cbce5e5e716a00d5342f570ad07cf3f2556
hazywho <116442947+hazywho@users.noreply.github.com>
2023-10-19 21:45:18 +0800

    m

--- Other Competitive programming/python/AvoidingZero.py
+++ Other Competitive programming/python/AvoidingZero.py
@@ -8,17 +8,28 @@ for _ in range(first):
         third.append(int(l))
     for n in third:
         count += n
+
+
     if count == 0:
         print("NO")
     else:
         print("YES")
         if count > 0:
             third.sort(reverse=True)
+
+
             for n in range(len(third)-1): print(third[n],end=" ")
             print(third[len(third)-1])
+
+
+
         else:
             third.sort(reverse=False)
+
+
             for n in range(len(third)-1): print(third[n],end=" ")
             print(third[len(third)-1])
+
+            
     count = 0
     third = []
