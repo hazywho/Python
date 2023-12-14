d7fdde9ad7417e415c9a208f7fa10bf0417777fd
hazywho <116442947+hazywho@users.noreply.github.com>
2023-12-14 11:36:24 +0800

    Update he.py
    may be final

--- copy of he.py
+++ copy of he.py
@@ -21,7 +21,7 @@
 
 # In[3]:
 import os
-path ="/home/hezy/Downloads/ht/images" # shows dialog box and return the path
+path ="/home/hezy/Downloads/ht/images" 
 print(path)
 os.chdir(path)
 from unittest.util import sorted_list_difference 
@@ -40,18 +40,12 @@ import math
 import tkinter
 from tkinter import messagebox
 import gradio as gr
-
-
+import re
 
 def variance_of_laplacian(image): 
     # compute the Laplacian of the image and then return the focus
     # measure, which is simply the variance of the Laplacian
     return cv2.Laplacian(image, cv2.CV_64F).var()
-    
-    
-    
-    
-    
 
 def run():
     path ="/home/hezy/Downloads/ht/images"
@@ -62,13 +56,13 @@ def run():
     distance = 80 
     mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
     rotation = 0
-    rs = 20
+    rs = 25
     while rotation <= rs:
         camera = cv2.VideoCapture(0)
         return_value, image = camera.read()
         cv2.imwrite(os.path.join(path,(str(rotation)+".jpg")), image)
         del(camera)
-        mymotortest.motor_go(0, "Full", 2, 0.005, False, 0.01)
+        mymotortest.motor_go(0, "Full", 1, 0.005, False, 0.01)
         image = cv2.imread(os.path.join(path,(str(rotation)+".jpg")))                      
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         fm = variance_of_laplacian(gray)
@@ -76,12 +70,10 @@ def run():
         num=str(round(fm*100)/100)+".jpg"
         cv2.imwrite(os.path.join(folder , num), image)
         rotation += 1
-    mymotortest.motor_go(1, "Full", rs*2, 0.005, False, 0.01)
+    mymotortest.motor_go(1, "Full", rs, 0.005, False, 0.01)
     
     return "images have been captured"
         
-
-    # In[4]:
 def show():
     path ="/home/hezy/Downloads/ht/placeholder"
     lst = []
@@ -94,20 +86,23 @@ def show():
             text = "Blurry"
         else:
             text = "Not Blurry"
-        # show the image
+        # save the image
+        cv2.imwrite(imagePath+"_o", image)
         cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
             cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 3)
-        cv2.imshow("Image", image)
-        key = cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        print(imagePath)
+        cv2.imwrite(imagePath, image)
+        
         lst.append(fm)
         
     #type in placeholder folder directory
     print(lst)
     folder = "/home/hezy/Downloads/ht/output"
+    file_name2 = str(max(lst))+".jpg_o"
     file_name = str(max(lst))+".jpg"
     print(file_name)
     new_name = os.path.join(folder, file_name)
+    new_name2 = os.path.join(folder, file_name2)
 
     dirthree = "/home/hezy/Downloads/ht/output"
     for filesthree in os.listdir(dirthree):
@@ -116,9 +111,13 @@ def show():
             shutil.rmtree(paththree)
         except OSError:
            os.remove(paththree)
-    shutil.move("/home/hezy/Downloads/ht/placeholder/" + file_name, new_name )
-    return new_name
-
+    shutil.copyfile("/home/hezy/Downloads/ht/placeholder/" + file_name, new_name )
+    shutil.copyfile("/home/hezy/Downloads/ht/placeholder/" + file_name2, new_name2 )
+    storage = []
+    for itemsPath in paths.list_images("/home/hezy/Downloads/ht/placeholder"):
+        storage.append(itemsPath)
+    print(storage)
+    return storage
     
 def delete():
     dir = "/home/hezy/Downloads/ht/placeholder"
@@ -138,27 +137,24 @@ def delete():
            os.remove(pathtwo)
     return "images have been deleted"
 
+def clear():
+    return None
+
 #FUNCTIONS END HERE.
     
 with gr.Blocks() as demo:
-    out1 = gr.Textbox(value = "", label = "IMG.cap Output")
-    out2 = gr.Textbox(value = "", label = "IMG.del Output")
-    imout1 = gr.Image()
-
+    out1 = gr.Textbox(value = "", label = "IMG.capture Log")
+    out2 = gr.Textbox(value = "", label = "IMG.delete Log")
+    gallery = gr.Gallery(show_label=True, elem_id = "gallery", object_fit="contain")
     btn = gr.Button(value = "Start Taking Image")
     btn.click(run,outputs=[out1])
+    showb = gr.Button(value = "Show image")
+    showb.click(show, outputs = [gallery])
     btn2 = gr.Button(value = "Delete")
     btn2.click(delete,outputs=[out2])
+    btn2.click(clear,outputs=[gallery])
     
-    showb = gr.Button(value = "Show img")
-    showb.click(show, outputs = [imout1])
+
 if __name__ == "__main__":
     demo.launch(show_api=False)
-# root = tkinter.Tk()
-# b1 = tkinter.Button(root,text="add",command= run)
-# b2 = tkinter.Button(root,text="show image",command= show)
-# b3 = tkinter.Button(root,text="delete images",command= delete)
-# b3.place(x=100,y=100)
-# b1.place(x=20,y=20)
-# b2.place(x=100,y=20)
-# root.mainloop()
+
