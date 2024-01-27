bef813d0c9d97cfbea320573f5668773d975934d
hazywho <116442947+hazywho@users.noreply.github.com>
2024-01-27 18:26:35 +0800

    dying

--- 2024/MCC/IWannaBeTheGuy.py
+++ 2024/MCC/IWannaBeTheGuy.py
@@ -20,3 +20,12 @@ if l != [1,2,3]:
 else:
     print("Oh, my keyboard!")
 
+
+level =  set(range(1, int(input()) + 1))
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+
+x = set(x[1:])
+y = set(y[1:])
+
+print('I become the guy.' if level - (x | y) == set() else 'Oh, my keyboard!')
