8200ee882dded52afe0f43c9562cc8c113280ce6
hazywho <116442947+hazywho@users.noreply.github.com>
2024-06-11 18:18:13 +0800

    newest

--- copy of he.py
+++ copy of he.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 # coding: utf-8
 
 # In[1]:
@@ -41,14 +41,30 @@ import tkinter
 from tkinter import messagebox
 import gradio as gr
 import re
+from ultralytics import YOLO
+
+model = YOLO(r"C:\Users\zanyi\OneDrive\Git hub\Ai\BAIR3\runs\detect\train9\weights\best.pt")
 
 def variance_of_laplacian(image): 
     # compute the Laplacian of the image and then return the focus
     # measure, which is simply the variance of the Laplacian
     return cv2.Laplacian(image, cv2.CV_64F).var()
 
+def recognise(path):
+    img = cv2.imread(path)
+    result = model.predict(img)
+    # Process results list
+    for result in results:
+        boxes = result.boxes  # Boxes object for bounding box outputs
+        masks = result.masks  # Masks object for segmentation masks outputs
+        keypoints = result.keypoints  # Keypoints object for pose outputs
+        probs = result.probs  # Probs object for classification outputs
+        obb = result.obb  # Oriented boxes object for OBB outputs
+        result.show()  # display to screen
+        result.save(filename="/home/hezy/Downloads/ht/output/final.jpg")  # save to disk
+        
 def run():
-    path ="/home/hezy/Downloads/ht/images"
+    path = "/home/hezy/Downloads/ht/images"
     
     GPIO_pins = (14,15,18)
     direction = 20
@@ -56,13 +72,13 @@ def run():
     distance = 80 
     mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
     rotation = 0
-    rs = 25
-    while rotation <= rs:
+    rs = 30
+    while rotation < rs:
         camera = cv2.VideoCapture(0)
         return_value, image = camera.read()
         cv2.imwrite(os.path.join(path,(str(rotation)+".jpg")), image)
         del(camera)
-        mymotortest.motor_go(0, "Full", 1, 0.005, False, 0.01)
+        mymotortest.motor_go(1, "Half", 1, 0.005, False, 0.01)
         image = cv2.imread(os.path.join(path,(str(rotation)+".jpg")))                      
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         fm = variance_of_laplacian(gray)
@@ -70,7 +86,7 @@ def run():
         num=str(round(fm*100)/100)+".jpg"
         cv2.imwrite(os.path.join(folder , num), image)
         rotation += 1
-    mymotortest.motor_go(1, "Full", rs, 0.005, False, 0.01)
+    mymotortest.motor_go(0, "Half", rs, 0.005, False, 0.01)
     
     return "images have been captured"
         
@@ -103,6 +119,9 @@ def show():
     print(file_name)
     new_name = os.path.join(folder, file_name)
     new_name2 = os.path.join(folder, file_name2)
+    
+    #recognise
+    recognise(new_name2)
 
     dirthree = "/home/hezy/Downloads/ht/output"
     for filesthree in os.listdir(dirthree):
@@ -151,10 +170,7 @@ with gr.Blocks() as demo:
     showb = gr.Button(value = "Show image")
     showb.click(show, outputs = [gallery])
     btn2 = gr.Button(value = "Delete")
-    btn2.click(delete,outputs=[out2])
-    btn2.click(clear,outputs=[gallery])
-    
-
-if __name__ == "__main__":
+    btn2.click(delete,outputs=out2)
+    btn2.click(delete,outputs=gallery) 
     demo.launch(show_api=False)
 
